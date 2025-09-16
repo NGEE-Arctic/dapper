6c0a1733bf3d6ea319559b525d84cb42c97c8ada
Jemma Stachelek <jemma.stachelek@gmail.com>
2025-09-16 12:43:23 -0600

    towards ci testing

---
+++ tests/test_cmip_utils.py
@@ -0,0 +1,6 @@
+from intake_esm.core import esm_datastore
+
+def test_intake:
+    col = intake.open_esm_datastore("https://storage.googleapis.com/cmip6/pangeo-cmip6.json")
+    assert isinstance(col, esm_datastore)
+
